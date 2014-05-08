3d140b10c5c8fe8bf6779b5e4a13088a1f19b3ff
Alexander Slansky <alexander@slansky.net>
2014-05-08 10:40:16 +0200

    adds search feature

--- index.js
+++ index.js
@@ -10,7 +10,12 @@
 }(function ($) {
   'use strict';
 
-  var defaults = {};
+  var defaults = {
+    langSingleDefaultText: 'Select an Option',
+    langMultiDefaultText: 'Select Some Options',
+    langNoSearchResultsText: 'No results match',
+    langMultiSelectedText: '%s and %d others selected'
+  };
 
   var SelectParser = (function() {
     function SelectParser() {
@@ -124,46 +129,69 @@
     this.settings = settings;
     this.$select = $ele;
     this.$container = null;
+    this.$search = [];
     this.open = false;
     this.settings.multi = !!this.$select.attr('multiple');
 
     if (!this.settings.placeholder) {
-      this.settings.placeholder = this.$select.attr('placeholder') || '&nbsp;';
+      this.settings.placeholder = this.$select.attr('placeholder') || (this.settings.multi) ? this.settings.langMultiSelectedText : this.settings.langSingleDefaultText;
     }
 
     this.render();
     this.initEvents();
   };
 
+  SelectKit.prototype.initEvents = function () {
+    var _this = this;
+    this.$container.find('.selectkit-display').bind('mousedown.selectkit', function (evt) {
+      _this.toggleDropdown(evt);
+    });
+
+    this.$container.find('.selectkit-reset').bind('mouseup.selectkit', function () {
+      _this.reset();
+    });
+    this.$list.delegate('.selectkit-choice', 'mouseup.selectkit', function (evt) {
+      _this.choiceMouseup(this, evt);
+    });
+    if (this.$search.length) {
+      this.$search.bind('keyup.selectkit', function(evt) {
+        _this.keydownCheck(evt);
+      });
+      this.$search.bind('keydown.selectkit', function(evt) {
+        _this.keydownCheck(evt);
+      });
+    }
+  };
+
   SelectKit.prototype.render = function () {
+    var width = this.getSelectWidth();
+    this.$select.hide();
     this.$select.wrap('<div class="selectkit"></div>');
     this.$container = this.$select.parent();
-    if (this.$select.width() && this.$select.width() > 0) {
-      this.$container.width(this.$select.width());
-    }
-
-    //this.$select.hide();
+    this.$container.css('width', width + 'px');
 
     this.$container.append('<div class="selectkit-display"><span>' + this.settings.placeholder + '</span><i class="selectkit-caret"></i></div>');
-    this.$display = $('.selectkit-display', this.$container);
-    this.$text = $('span', this.$display);
+    this.$display = this.$container.find('.selectkit-display');
+    this.$text = this.$display.find('span');
 
     this.$container.append('<div class="selectkit-list"></div>');
-    this.$dropdown = $('.selectkit-list', this.$container);
+    this.$dropdown = this.$container.find('.selectkit-list');
 
     if (this.settings.search) {
       this.$dropdown.append('<div class="selectkit-search"><input type="text" name="selectkit-search"></div>');
+      this.$search = this.$dropdown.find('input');
     }
 
     this.$dropdown.append('<ul class="selectkit-choices"></ul>');
-    this.$list = $('.selectkit-choices', this.$container);
+    this.$list = this.$container.find('.selectkit-choices');
 
+    this.data = SelectParser.select_to_array(this.$select.get(0));
     this.$choices = this.renderList();
   };
 
   SelectKit.prototype.renderList = function () {
     var _this = this;
-    this.data = SelectParser.select_to_array(this.$select.get(0));
+    this.$list.empty();
     $.each(this.data, function () {
       if (this.group) {
         _this.renderGroup(this);
@@ -172,11 +200,11 @@
         _this.renderOption(this);
       }
     });
-    return $('.selectkit-choice', this.$container);
+    return this.$container.find('.selectkit-choice');
   };
 
   SelectKit.prototype.renderGroup = function (data) {
-    var $ele = $('<li class="selectkit-group">' + data.label + '</li>');
+    var $ele = $('<li class="selectkit-group" data-group-index="' + data.array_index + '">' + data.label + '</li>');
     $ele.data().data = data;
     this.$list.append($ele);
   };
@@ -187,8 +215,13 @@
       if (this.settings.reset) {
         this.$display.append('<i class="selectkit-reset selectkit-reset-disabled"></i>');
       }
+      return '';
     }
     else {
+      if (data.search_match === false) {
+        return '';
+      }
+
       var $ele = $('<li class="selectkit-choice">' + data.html + '</li>');
       if (this.settings.multi && this.settings.checkbox) {
         $ele.prepend('<input type="checkbox" class="selectkit-check">');
@@ -197,24 +230,19 @@
         $ele.find('input').prop('disabled', true);
         $ele.addClass('selectkit-disabled');
       }
+      if (data.selected) {
+        $ele.find('input').prop('checked', true);
+        $ele.addClass('selectkit-selected');
+      }
+      if (data.group_array_index) {
+        $ele.addClass('selectkit-group-item');
+        $ele.attr('data-group-index',  data.group_array_index);
+      }
       data.$ele = $ele;
       $ele.data().data = data;
       this.$list.append($ele);
+      return $ele;
     }
-  };
-
-  SelectKit.prototype.initEvents = function () {
-    var _this = this;
-    $('.selectkit-display', this.$container).bind('mousedown.selectkit', function (evt) {
-      _this.toggleDropdown(evt);
-    });
-
-    $('.selectkit-reset', this.$container).bind('mouseup.selectkit', function (evt) {
-      _this.reset();
-    });
-    this.$choices.bind('mouseup.selectkit', function (evt) {
-      _this.choiceMouseup(this, evt);
-    });
   };
 
   SelectKit.prototype.toggleDropdown = function (evt) {
@@ -290,26 +318,141 @@
     this.setText();
   };
 
-  SelectKit.prototype.setText = function () {
-    var $selected = this.$choices.filter('.selectkit-selected');
-    if ($selected.length === 1) {
-      this.$text.html($selected.data().data.html);
-    }
-    else if ($selected.length > 1) {
-      this.$text.html($selected.data().data.html + ' and ' + ($selected.length - 1) + ' others selected');
-    }
-    else {
-      this.$text.html(this.settings.placeholder);
+  SelectKit.prototype.keydownCheck = function (evt) {
+    var stroke, _ref1;
+    stroke = (_ref1 = evt.which) != null ? _ref1 : evt.keyCode;
+    switch (stroke) {
+      case 13:
+      case 38:
+      case 40:
+      case 9:
+      case 16:
+      case 91:
+      case 17:
+        evt.preventDefault();
+        break;
+      default:
+        return this.searchResults();
     }
   };
 
+  SelectKit.prototype.setText = function (text) {
+    var result = '';
+    if (text) {
+      result = text;
+    }
+    else {
+      var selected = $.grep(this.data, function (a) {
+        return a.selected;
+      });
+      if (selected.length === 1) {
+        result = selected[0].html;
+      }
+      else if (selected.length > 1) {
+        result = SelectKit.formatString(this.settings.langMultiSelectedText, selected[0].html, selected.length - 1);
+      }
+      else {
+        result = this.settings.placeholder;
+      }
+    }
+    this.$text.html(result);
+    return result;
+  };
+
+  SelectKit.prototype.searchResults = function () {
+    var escapedSearchText, option, regex, regexAnchor, results, results_group, searchText, startpos, text, zregex, _i, _len, _ref;
+    this.clearNoResult();
+    results = 0;
+    searchText = this.getSearchText();
+    escapedSearchText = searchText.replace(/[-[\]{}()*+?.,\\^$|#\s]/g, '\\$&');
+    regexAnchor = this.settings.searchContains ? '' : '^';
+    regex = new RegExp(regexAnchor + escapedSearchText, 'i');
+    zregex = new RegExp(escapedSearchText, 'i');
+    _ref = this.data;
+    for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+      option = _ref[_i];
+      option.search_text = option.html;
+      option.search_match = false;
+      results_group = null;
+      if (this.includeOptionInResults(option)) {
+        option.search_match = this.stringMatch(option.search_text, regex);
+        if (option.search_match) {
+          results += 1;
+          if (searchText.length) {
+            startpos = option.search_text.search(zregex);
+            text = option.search_text.substr(0, startpos + searchText.length) + '</em>' + option.search_text.substr(startpos + searchText.length);
+            option.search_text = text.substr(0, startpos) + '<em>' + text.substr(startpos);
+          }
+          option.search_match = true;
+        }
+        else {
+          option.search_match = false;
+        }
+      }
+      else {
+        option.search_match = false;
+      }
+    }
+    if (results < 1 && searchText.length) {
+      return this.setNoResult();
+    } else {
+      this.renderList();
+    }
+  };
+
+  SelectKit.prototype.clearNoResult = function () {
+    this.$list.find('.no-results').remove();
+  };
+
+  SelectKit.prototype.setNoResult = function () {
+    this.$list.empty();
+    return this.$list.append('<li class="no-results">' + this.settings.langNoSearchResultsText + '</li>');
+  };
+
+  SelectKit.prototype.stringMatch = function(search_string, regex) {
+    var part, parts, _i, _len;
+    if (regex.test(search_string)) {
+      return true;
+    } else if (search_string.indexOf(' ') >= 0 || search_string.indexOf('[') === 0) {
+      parts = search_string.replace(/\[|\]/g, '').split(' ');
+      if (parts.length) {
+        for (_i = 0, _len = parts.length; _i < _len; _i++) {
+          part = parts[_i];
+          if (regex.test(part)) {
+            return true;
+          }
+        }
+      }
+    }
+  };
+
+  SelectKit.prototype.includeOptionInResults = function(option) {
+    if (option.disabled) {
+      return false;
+    }
+    if (option.group) {
+      return false;
+    }
+    if (option.empty) {
+      return false;
+    }
+    return true;
+  };
+
+
   SelectKit.prototype.reset = function () {
-    this.$select.find(':selected').each(function () {
-      this.selected = false;
+    $.each(this.data, function (i, item) {
+      item.selected = undefined;
+      item.search_match = undefined;
     });
+    this.renderList();
+    this.setText();
+    if (this.settings.multi) {
+      this.$select.find(':selected').each(function () {
+        this.selected = false;
+      });
+    }
     this.$select.find('option').get(this.empty_choice.options_index).selected = true;
-    this.$list.find('.selectkit-selected').removeClass('selectkit-selected');
-    this.$choices.find('input').prop('checked', false);
     this.$display.find('.selectkit-reset').addClass('selectkit-reset-disabled');
     this.hide();
   };
@@ -318,6 +461,18 @@
     this.$select.show();
     this.$container.before(this.$select);
     this.$container.remove();
+  };
+
+  SelectKit.prototype.getSelectWidth = function() {
+    if (this.settings.width != null) {
+      return this.settings.width;
+    } else {
+      return this.$select.get(0).offsetWidth;
+    }
+  };
+
+  SelectKit.prototype.getSearchText = function() {
+    return (this.$search.val() === this.placeholder) ? '' : $('<div/>').text($.trim(this.$search.val())).html();
   };
 
   SelectKit.testActive = function (evt, scope) {
@@ -330,7 +485,7 @@
     }
   };
 
-  SelectKit.isBrowserSupported = function() {
+  SelectKit.isBrowserSupported = function () {
     if (window.navigator.appName === 'Microsoft Internet Explorer') {
       return document.documentMode >= 8;
     }
@@ -343,6 +498,19 @@
       }
     }
     return true;
+  };
+
+  SelectKit.formatString = function () {
+    var a, b, c;
+    a = arguments[0];
+    b = [];
+    for(c = 1; c < arguments.length; c++) {
+      b.push(arguments[c]);
+    }
+    for(c in b) {
+      a = a.replace(/%[a-z]/,b[c]);
+    }
+    return a;
   };
 
   $.fn.selectKit = function (options) {
